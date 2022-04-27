b20c853f715f1b19f75faf67bca7d413899059d2
Jan Kubowicz <83781535+ZasaQ@users.noreply.github.com>
2022-04-28 00:26:04 +0200

    Add files via upload

---
+++ Salon-Samochodowy-main/src/Menu.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Menu extends Samochody{
+    Menu(String id_samochodu,String marka,String model,String kolor,String rocznik,String cena) {
+        super(id_samochodu, marka, model, kolor, rocznik, cena);
+    }
+
+
+}
