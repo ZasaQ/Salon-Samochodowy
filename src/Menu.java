85ded0b2cbd67048d5dad76610566728af0b578a
Jan Kubowicz <83781535+ZasaQ@users.noreply.github.com>
2022-04-28 00:28:47 +0200

    Delete src directory

--- src/Menu.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Menu extends Samochody{
-    Menu(String id_samochodu,String marka,String model,String kolor,String rocznik,String cena) {
-        super(id_samochodu, marka, model, kolor, rocznik, cena);
-    }
-    public void
-
-}
